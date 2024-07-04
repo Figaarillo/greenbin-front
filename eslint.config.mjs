51f530b701e9d8c55bcf10b77c48058ff3776a77
Figaarillo <axel.leonardi.22@gmail.com>
2024-07-03 21:48:58 -0300

    feat: install and config eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,37 @@
+import config from "eslint-config-standard";
+
+export default [
+  ...[].concat(config),
+  {
+    rules: {
+      indent: ["error", 2],
+      "linebreak-style": ["error", "unix"],
+      quotes: ["error", "single"],
+      semi: ["error", "never"],
+      eqeqeq: "error",
+      "no-trailing-spaces": "error",
+      "object-curly-spacing": ["error", "always"],
+
+      "arrow-spacing": [
+        "error",
+        {
+          before: true,
+          after: true,
+        },
+      ],
+
+      "no-console": [
+        "warn",
+        {
+          allow: ["error"],
+        },
+      ],
+
+      "space-before-function-paren": ["error", "always"],
+
+      "max-len": "off",
+
+      "object-curly-spacing": ["error", "always"],
+    },
+  },
+];
