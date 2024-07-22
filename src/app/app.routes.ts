b2b03d1bd5f91581da17e70145d42e3b61fdd1d6
wink15 <santigiopo3@gmail.com>
2024-07-22 10:29:21 -0300

    feat: adding consultar-entidad component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
+import { ConsultarEntidadComponent } from './pages/consultar-entidad/consultar-entidad.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'consultar',
+    loadComponent: () =>
+      import('./pages/consultar-entidad/consultar-entidad.component').then(m => m.ConsultarEntidadComponent)
+  }
+]
---
+++ src/app/pages/consultar-entidad/consultar-entidad.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-consultar-entidad',
+  standalone: true,
+  imports: [],
+  templateUrl: './consultar-entidad.component.html',
+  styleUrl: './consultar-entidad.component.scss'
+})
+export class ConsultarEntidadComponent {}
