bb724d050c1a10e3102484aaf9de8d57d1a2d858
wink15 <santigiopo3@gmail.com>
2024-08-29 09:37:52 -0300

    feat: adding store services and component

---
+++ src/app/pages/registrar-local/registrar-local.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-registrar-local',
+  standalone: true,
+  imports: [],
+  templateUrl: './registrar-local.component.html',
+  styleUrl: './registrar-local.component.scss'
+})
+export class RegistrarLocalComponent {}
