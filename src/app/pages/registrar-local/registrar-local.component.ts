0431c40f2449e2d87beae1b57ba385413887b1ee
wink15 <santigiopo3@gmail.com>
2024-08-29 21:39:19 -0300

    feat: adding formbuilder and validators

--- src/app/pages/registrar-local/registrar-local.component.ts
+++ src/app/pages/registrar-local/registrar-local.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core'
-import { FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms'
+import { Component, inject } from '@angular/core'
+import { FormBuilder, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms'
 import { MatButton } from '@angular/material/button'
 import { MatCardModule } from '@angular/material/card'
 import { MatFormFieldModule } from '@angular/material/form-field'
@@ -24,4 +24,27 @@ import { MatToolbar } from '@angular/material/toolbar'
   templateUrl: './registrar-local.component.html',
   styleUrl: './registrar-local.component.scss'
 })
-export class RegistrarLocalComponent {}
+export class RegistrarLocalComponent {
+  title: string = 'Registrarse'
+  private readonly _formBuilder = inject(FormBuilder)
+  formGroup = this._formBuilder.nonNullable.group({
+    name: ['', Validators.required],
+    address: ['', Validators.required],
+    cuit: ['', Validators.required],
+    username: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    phoneNumber: ['', [Validators.required, Validators.pattern('^[0-9]{10,15}$')]],
+    password: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern(
+          /^(?=.*[A-Z])(?=.*[!@#$%^&*()_+{}\[\]:;"'<>,.?/~`])[A-Za-z\d!@#$%^&*()_+{}\[\]:;"'<>,.?/~`]{8,}$/
+        )
+      ]
+    ],
+    confirmPassword: ['', Validators.required]
+  })
+
+  register(): void {}
+}
