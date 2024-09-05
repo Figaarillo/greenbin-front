2b3a509715abf570b528ac006bacbcee3df210a7
matias-oggero <matias-oggero@hotmail.com>
2024-09-05 16:54:04 -0300

    feat: create form

--- src/app/pages/modificar-vecino/modificar-vecino.component.ts
+++ src/app/pages/modificar-vecino/modificar-vecino.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-modificar-vecino',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core'
   templateUrl: './modificar-vecino.component.html',
   styleUrl: './modificar-vecino.component.scss'
 })
-export class ModificarVecinoComponent {}
+export class ModificarVecinoComponent {
+  form: FormGroup
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      firstname: ['', [Validators.required, Validators.minLength(2)]],
+      lastname: ['', [Validators.required, Validators.minLength(2)]],
+      username: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(20)]],
+      email: ['', [Validators.required, Validators.email]],
+      phoneNumber: ['', [Validators.required, Validators.pattern('^[0-9]{10,15}$')]]
+    })
+  }
+}
