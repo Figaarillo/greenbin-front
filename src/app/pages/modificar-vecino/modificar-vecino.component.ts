fe59834a49009c8fc15db6cb4d694f34d97aa918
matias-oggero <matias-oggero@hotmail.com>
2024-09-05 18:15:06 -0300

    feat: form styles

--- src/app/pages/modificar-vecino/modificar-vecino.component.ts
+++ src/app/pages/modificar-vecino/modificar-vecino.component.ts
@@ -8,6 +8,7 @@ import { ActivatedRoute, Router, RouterModule } from '@angular/router'
 import { NavbarComponent } from '../../components/navbar/navbar.component'
 import { VecinoService } from '../../services/vecino/vecino.service'
 import { Vecino } from '../../services/interfaces/vecino'
+import { CommonModule } from '@angular/common'
 
 @Component({
   selector: 'app-modificar-vecino',
@@ -20,7 +21,8 @@ import { Vecino } from '../../services/interfaces/vecino'
     MatInputModule,
     ReactiveFormsModule,
     MatButtonModule,
-    RouterModule
+    RouterModule,
+    CommonModule
   ],
   templateUrl: './modificar-vecino.component.html',
   styleUrl: './modificar-vecino.component.scss'
