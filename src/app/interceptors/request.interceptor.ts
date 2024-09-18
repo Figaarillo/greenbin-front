e32f8a1f6130585c05c0f8dac29c87a7cac645a1
matias-oggero <matias-oggero@hotmail.com>
2024-09-18 00:48:33 -0300

    fix: acentos y estilos de form

--- src/app/interceptors/request.interceptor.ts
+++ src/app/interceptors/request.interceptor.ts
@@ -19,14 +19,14 @@ export const requestInterceptor: HttpInterceptorFn = (req, next) => {
       url: '/api/neighbor/signup',
       method: 'POST',
       errorMessage: 'Error al crear el vecino, por favor revise los datos y vuelva a intentarlo',
-      successMessage: 'El vecino se ha creado con exito',
+      successMessage: 'El vecino se ha creado con éxito',
       routeToNavigate: ''
     },
     {
       url: '/api/neighbor',
       method: 'PUT',
       errorMessage: 'Error al editar el vecino, por favor revise los datos y vuelva a intentarlo',
-      successMessage: 'El vecino se ha editado con exito',
+      successMessage: 'El vecino se ha editado con éxito',
       routeToNavigate: ''
     }
   ]
