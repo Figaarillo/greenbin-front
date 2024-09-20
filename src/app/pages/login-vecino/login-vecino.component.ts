6a53a0980f7520d22691bd26c5828b21ca541b51
matias-oggero <matias-oggero@hotmail.com>
2024-09-20 15:58:09 -0300

    feat: componente y route

---
+++ src/app/pages/login-vecino/login-vecino.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-login-vecino',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-vecino.component.html',
+  styleUrl: './login-vecino.component.scss'
+})
+export class LoginVecinoComponent {}
