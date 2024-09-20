0b66c74c8a73d8295b05ba0c6dab8a01873ba07e
matias-oggero <matias-oggero@hotmail.com>
2024-09-20 16:44:22 -0300

    feat: maquetacion login form

--- src/app/pages/login-vecino/login-vecino.component.ts
+++ src/app/pages/login-vecino/login-vecino.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core'
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { MatInputModule } from '@angular/material/input'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { FormsModule } from '@angular/forms'
+import { MatCheckboxModule } from '@angular/material/checkbox'
 
 @Component({
   selector: 'app-login-vecino',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatButtonModule, MatInputModule, MatFormFieldModule, FormsModule, MatCheckboxModule],
   templateUrl: './login-vecino.component.html',
   styleUrl: './login-vecino.component.scss'
 })
