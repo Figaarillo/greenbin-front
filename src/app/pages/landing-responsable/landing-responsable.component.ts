404801ff5fc50a609c0324430142342a60c58ec1
matias-oggero <matias-oggero@hotmail.com>
2024-10-04 12:21:18 -0300

    feat: create component and route

---
+++ src/app/pages/landing-responsable/landing-responsable.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-landing-responsable',
+  standalone: true,
+  imports: [],
+  templateUrl: './landing-responsable.component.html',
+  styleUrl: './landing-responsable.component.scss'
+})
+export class LandingResponsableComponent {}
