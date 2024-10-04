b702580aeb9381cbf83226cc7289cb61931b110f
matias-oggero <matias-oggero@hotmail.com>
2024-10-04 12:53:08 -0300

    feat: maquetacion

--- src/app/pages/landing-responsable/landing-responsable.component.ts
+++ src/app/pages/landing-responsable/landing-responsable.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core'
+import { FormControl, ReactiveFormsModule } from '@angular/forms'
+import { MatButtonModule } from '@angular/material/button'
+import { MatDividerModule } from '@angular/material/divider'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatIconModule } from '@angular/material/icon'
+import { MatListModule } from '@angular/material/list'
+import { MatSelectModule } from '@angular/material/select'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatToolbarModule } from '@angular/material/toolbar'
 
 @Component({
   selector: 'app-landing-responsable',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatDividerModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './landing-responsable.component.html',
   styleUrl: './landing-responsable.component.scss'
 })
-export class LandingResponsableComponent {}
+export class LandingResponsableComponent {
+  listPtoVerde: any[] = [
+    { id: 'steak-0', name: 'Escuela Juan Castillo' },
+    { id: 'pizza-1', name: 'Cooperativa' },
+    { id: 'tacos-2', name: 'Municerca' }
+  ]
+  ptoVerde = new FormControl(this.listPtoVerde[2].id)
+}
