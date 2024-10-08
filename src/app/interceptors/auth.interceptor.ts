c7a31e6c894f2ed479c9709487cf93beb854c675
matias-oggero <matias-oggero@hotmail.com>
2024-10-08 08:28:21 -0300

    feat: interceptor para guardar en localstorage los tokens y datos del usuario con la sesion actual

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -1,5 +1,28 @@
-import { HttpInterceptorFn } from '@angular/common/http'
+import { HttpInterceptorFn, HttpResponse } from '@angular/common/http'
+import { SesionService } from '../services/sesion/sesion.service'
+import { inject } from '@angular/core'
+import { tap, catchError, throwError } from 'rxjs'
+import { LoginResponse } from '../services/interfaces/login-response'
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req)
+  const sesionService = inject(SesionService)
+  return next(req).pipe(
+    tap({
+      next: event => {
+        if (event instanceof HttpResponse && req.url.includes('/auth/login')) {
+          // Aquí obtienes el objeto de la respuesta
+          const loginData = event.body as any // Cambia esto al tipo que esperas
+          sesionService.setLoginData(<LoginResponse>loginData.data)
+        }
+      },
+      error: err => {
+        // Manejo del error si es necesario
+        console.error(err)
+      }
+    }),
+    catchError((err: any) => {
+      //aca deberia tratar de usar el refresh login creo nose
+      return throwError(() => err)
+    })
+  )
 }
