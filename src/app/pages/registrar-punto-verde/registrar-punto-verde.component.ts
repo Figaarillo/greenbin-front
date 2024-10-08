447b04f2a4c544573f941c52ec802ad987addd95
matias-oggero <matias-oggero@hotmail.com>
2024-10-08 11:39:28 -0300

    feat: componente registrar punto verde

---
+++ src/app/pages/registrar-punto-verde/registrar-punto-verde.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-registrar-punto-verde',
+  standalone: true,
+  imports: [],
+  templateUrl: './registrar-punto-verde.component.html',
+  styleUrl: './registrar-punto-verde.component.scss'
+})
+export class RegistrarPuntoVerdeComponent {}
