019d3040cddcabf30e6d09b356f36fff559502d3
matias-oggero <matias-oggero@hotmail.com>
2024-10-14 10:51:48 -0300

    feat: create component and install googlemaps

---
+++ src/app/components/map-view/map-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-map-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './map-view.component.html',
+  styleUrl: './map-view.component.scss'
+})
+export class MapViewComponent {}
