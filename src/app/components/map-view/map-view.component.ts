d4bc180c1122406906f8b1fb4acfac48c769b03f
matias-oggero <matias-oggero@hotmail.com>
2024-10-14 12:56:11 -0300

    feat: create map with coords input

--- src/app/components/map-view/map-view.component.ts
+++ src/app/components/map-view/map-view.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core'
+import { GoogleMapsModule } from '@angular/google-maps'
 
 @Component({
   selector: 'app-map-view',
   standalone: true,
-  imports: [],
+  imports: [GoogleMapsModule],
   templateUrl: './map-view.component.html',
   styleUrl: './map-view.component.scss'
 })
-export class MapViewComponent {}
+export class MapViewComponent {
+  position = {
+    lat: 12.31,
+    lng: 23.44
+  }
+  label = {
+    color: 'green',
+    text: 'Punto Verde'
+  }
+  display: any
+  center: google.maps.LatLngLiteral = {
+    lat: 22.2736308,
+    lng: 70.7512555
+  }
+  zoom = 6
+  selectCoords(event: google.maps.MapMouseEvent) {
+    if (event.latLng != null) this.position = event.latLng.toJSON()
+  }
+}
