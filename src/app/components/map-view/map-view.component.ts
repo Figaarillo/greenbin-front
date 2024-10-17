50b5da3de28fb0a0a64a2f748245377a09abcbfe
matias-oggero <matias-oggero@hotmail.com>
2024-10-17 12:11:35 -0300

    feat: confia

--- src/app/components/map-view/map-view.component.ts
+++ src/app/components/map-view/map-view.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'
+import { Component, Input } from '@angular/core'
 import { GoogleMapsModule } from '@angular/google-maps'
 
 @Component({
@@ -8,22 +8,4 @@ import { GoogleMapsModule } from '@angular/google-maps'
   templateUrl: './map-view.component.html',
   styleUrl: './map-view.component.scss'
 })
-export class MapViewComponent {
-  position = {
-    lat: 12.31,
-    lng: 23.44
-  }
-  label = {
-    color: 'green',
-    text: 'Punto Verde'
-  }
-  display: any
-  center: google.maps.LatLngLiteral = {
-    lat: 22.2736308,
-    lng: 70.7512555
-  }
-  zoom = 6
-  selectCoords(event: google.maps.MapMouseEvent) {
-    if (event.latLng != null) this.position = event.latLng.toJSON()
-  }
-}
+export class MapViewComponent {}
