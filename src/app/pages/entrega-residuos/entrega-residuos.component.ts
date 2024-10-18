1577e6fe5eb5883f3d03da1da6787178c6d20aef
wink15 <santigiopo3@gmail.com>
2024-10-18 09:15:25 -0300

    feat: dni validator

--- src/app/pages/entrega-residuos/entrega-residuos.component.ts
+++ src/app/pages/entrega-residuos/entrega-residuos.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'
+import { Component, Inject, inject } from '@angular/core'
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
 import { MatButtonModule } from '@angular/material/button'
 import { MatFormFieldModule } from '@angular/material/form-field'
@@ -8,6 +8,8 @@ import { NavbarComponent } from '../../components/navbar/navbar.component'
 import { MatDividerModule } from '@angular/material/divider'
 import { MatChipsModule } from '@angular/material/chips'
 import { MatSelectModule } from '@angular/material/select'
+import Swal from 'sweetalert2'
+import { VecinoService } from '../../services/vecino/vecino.service'
 
 @Component({
   selector: 'app-entrega-residuos',
@@ -27,6 +29,7 @@ import { MatSelectModule } from '@angular/material/select'
   styleUrl: './entrega-residuos.component.scss'
 })
 export class EntregaResiduosComponent {
+  dniValidated = false
   categorias: any[] = [
     {
       id: '1',
@@ -50,14 +53,54 @@ export class EntregaResiduosComponent {
     }
   ]
   form!: FormGroup
+  dniValidator!: FormGroup
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private vecinoService: VecinoService
+  ) {
+    this.dniValidator = this.fb.group({
+      dni: ['', [Validators.required]]
+    })
     this.form = this.fb.group({
-      dni: ['', [Validators.required]],
       categoria: [{}],
       kilos: ['']
     })
   }
 
   onSubmit() {}
+
+  validateDni() {
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success ',
+        cancelButton: 'btn btn-danger'
+      }
+    })
+    if (this.dniValidator.valid) {
+      const dni = this.dniValidator.value.dni
+      Swal.fire({
+        title: 'Cargando',
+        showConfirmButton: false,
+        allowOutsideClick: false,
+        allowEscapeKey: false
+      })
+
+      Swal.showLoading()
+
+      this.vecinoService.validateDni(dni).subscribe(
+        res => {
+          Swal.close()
+          this.dniValidated = true
+        },
+        err => {
+          Swal.close()
+          swalWithBootstrapButtons.fire({
+            title: 'Ha ocurrido un error al validar su cuit.',
+            icon: 'error'
+          })
+        }
+      )
+    }
+  }
 }
