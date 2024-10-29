0a3661f64ba0b44a3a26ead5508294f500a4bf38
matias-oggero <matias-oggero@hotmail.com>
2024-10-29 11:14:22 -0300

    fix: correccion de logica de interceptor, add auth header

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -1,11 +1,14 @@
 import { HttpInterceptorFn, HttpResponse } from '@angular/common/http'
 import { SesionService } from '../services/sesion/sesion.service'
 import { inject } from '@angular/core'
-import { tap, catchError, throwError } from 'rxjs'
+import { tap } from 'rxjs'
 import { LoginResponse } from '../services/interfaces/login-response'
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const sesionService = inject(SesionService)
+
+  //si no es una ruta de registro, o peticion de refresh-token, se agrega el encabezado con el bearerToken
+
   return next(req).pipe(
     tap({
       next: event => {
@@ -13,16 +16,18 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
           // Aquí obtienes el objeto de la respuesta
           const loginData = event.body as any // Cambia esto al tipo que esperas
           sesionService.setLoginData(<LoginResponse>loginData.data)
+        } else {
+          //agregar el token al header
+          const token = sesionService.getAccessToken()
+          if (token) {
+            req = req.clone({
+              setHeaders: {
+                Authorization: `Bearer ${token}`
+              }
+            })
+          }
         }
-      },
-      error: err => {
-        // Manejo del error si es necesario
-        console.error(err)
       }
-    }),
-    catchError((err: any) => {
-      //aca deberia tratar de usar el refresh login creo nose
-      return throwError(() => err)
     })
   )
 }
