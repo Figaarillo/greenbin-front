024c5245b877373a6dd5e074ea15f88d851816a2
matias-oggero <matias-oggero@hotmail.com>
2024-10-29 11:48:12 -0300

    fix: simplificacion de logica, solo agregar token

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -2,7 +2,6 @@ import { HttpInterceptorFn, HttpResponse } from '@angular/common/http'
 import { SesionService } from '../services/sesion/sesion.service'
 import { inject } from '@angular/core'
 import { tap } from 'rxjs'
-import { LoginResponse } from '../services/interfaces/login-response'
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const sesionService = inject(SesionService)
@@ -12,12 +11,7 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
   return next(req).pipe(
     tap({
       next: event => {
-        if (event instanceof HttpResponse && req.url.includes('/auth/login')) {
-          // Aquí obtienes el objeto de la respuesta
-          const loginData = event.body as any // Cambia esto al tipo que esperas
-          sesionService.setLoginData(<LoginResponse>loginData.data)
-        } else {
-          //agregar el token al header
+        if (event instanceof HttpResponse && !req.url.includes('/auth/login')) {
           const token = sesionService.getAccessToken()
           if (token) {
             req = req.clone({
