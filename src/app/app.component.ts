1ed883248c0862f089e2cbf065280945ee5459ee
matias-oggero <matias-oggero@hotmail.com>
2024-10-28 22:02:05 -0300

    feat: interceptor que muestra el spinner cuando se esta realizando una peticion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
 import { NavbarComponent } from './components/navbar/navbar.component'
+import { LoaderComponent } from './components/loader/loader.component'
 NavbarComponent
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
