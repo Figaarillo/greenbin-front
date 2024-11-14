ce388c6cd115fc942e8ce11e98d2a71483a90333
matias-oggero <matias-oggero@hotmail.com>
2024-11-14 12:50:33 -0300

    feat: maquetacion general no responsive

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core'
+import { MatIconModule } from '@angular/material/icon'
 import { ActivatedRoute, RouterModule, Router } from '@angular/router'
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, MatIconModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
