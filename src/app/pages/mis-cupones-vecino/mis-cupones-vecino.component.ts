7ce719cc64d41a91f3a239c79f55f85c855cff80
matias-oggero <matias-oggero@hotmail.com>
2024-11-21 21:00:35 -0300

    feat: creacion y route de componente

---
+++ src/app/pages/mis-cupones-vecino/mis-cupones-vecino.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-mis-cupones-vecino',
+  standalone: true,
+  imports: [],
+  templateUrl: './mis-cupones-vecino.component.html',
+  styleUrl: './mis-cupones-vecino.component.scss'
+})
+export class MisCuponesVecinoComponent {}
