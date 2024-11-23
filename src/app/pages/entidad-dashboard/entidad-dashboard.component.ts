01e32fb38d4d288973a15463c715c6e2512f2c85
wink15 <santigiopo3@gmail.com>
2024-11-23 16:53:17 -0300

    feat: stats

--- src/app/pages/entidad-dashboard/entidad-dashboard.component.ts
+++ src/app/pages/entidad-dashboard/entidad-dashboard.component.ts
@@ -16,17 +16,29 @@ export class EntidadDashboardComponent implements OnInit {
   router = inject(Router)
   email = ''
   entidadServ = inject(EntidadService)
-  entidadId: string = ''
+  iframeUrl: string = ''
 
+  entidadId: string = '80468942-b86a-43e7-b530-9485310f871a'
   ngOnInit(): void {
     const info = localStorage.getItem('entidadInfo') || ''
     const entidadInfo = JSON.parse(info)
     this.email = entidadInfo.email
     this.name = entidadInfo.name
     this.entidadId = entidadInfo.id
+    this.loadIframeUrl(this.entidadId)
     console.log(this.entidadId)
   }
 
+  loadIframeUrl(id: string): void {
+    this.entidadServ.getMetabaseIframeUrl(id).subscribe({
+      next: response => {
+        this.iframeUrl = response.iframeUrl
+      },
+      error: err => {
+        console.error('Error al obtener el iframe URL:', err)
+      }
+    })
+  }
   logOut() {
     localStorage.removeItem('accessToken')
     localStorage.removeItem('refreshToken')
